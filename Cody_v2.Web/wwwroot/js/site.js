411a1a5534d5a9e85641c3221cad6f9983bb7b96
dinhtona <32251215+dinhtona@users.noreply.github.com>
2022-05-28 11:58:43 +0700

    Handling onsubmit form show loading pannel

--- Cody_v2.Web/wwwroot/js/site.js
+++ Cody_v2.Web/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function formSubmitted() {
+    $(':input[type="submit"]').prop('disabled', true);
+    $("#loading-spinner").show();
+    $("#fullscreenloading")?.show();
+}
